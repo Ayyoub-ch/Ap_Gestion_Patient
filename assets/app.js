2e4889bbdf420cef79b1509474066cf7f5750f74
arayan48 <arabirayanma@gmail.com>
2025-11-18 14:25:36 +0100

    ajout du header.css et aussi la partie mobile

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,6 @@ import './stimulus_bootstrap.js';
  */
 import './styles/app.css';
 import './styles/header.css';
+import './styles/mobile/header-mobile.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
