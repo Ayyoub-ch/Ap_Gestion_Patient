911b13d4259ede37fc364ebe2bf47b12ffcfcc20
Ayyoub <77174228+Ayyoub-ch@users.noreply.github.com>
2025-12-01 20:04:37 +0100

    Enhance patient management in AdministratifController
    Added patient management functionalities including retrieval, deletion, and modification.

--- src/Controller/AdministratifController.php
+++ src/Controller/AdministratifController.php
@@ -5,21 +5,65 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Patient;
+use Doctrine\Persistence\ManagerRegistry;
 
 final class AdministratifController extends AbstractController
 {
-    #[Route('/administratif', name: 'app_administratif')]
+    #[Route('/', name: 'app_administratif')]
     public function index(): Response
     {
         return $this->render('administratif/index.html.twig');
     }
-
+    
+    //Partie Patient
     #[Route('/patient', name: 'app_patient')]
-    public function patients(): Response
+    public function patients(ManagerRegistry $doctrine): Response
     {
-        return $this->render('administratif/patient.html.twig');
+        $repository = $doctrine->getRepository(Patient::class);
+        $patients = $repository->findAll(); // Récupère tous les patients
+        return $this->render('administratif/patient.html.twig', [
+            'patients' => $patients,
+        ]);
     }
 
+    /*Supprimer un patient*/
+    #[Route('/retirerPatient/{id}', name: 'app_retirer_patient', methods: ['POST'])]
+    public function retirerPatient(ManagerRegistry $doctrine, $id): Response
+    {
+        $repository = $doctrine->getRepository(Patient::class);
+        $em = $doctrine->getManager();
+
+        // Récupération du patient à supprimer
+        $patient = $repository->find($id);
+
+        if ($patient) {
+            $em->remove($patient);
+            $em->flush();
+
+            $this->addFlash('success', 'Le patient a été supprimé avec succès.');
+        } else {
+            $this->addFlash('error', 'Aucun patient trouvé avec cet ID.');
+        }
+
+        // Redirection vers la liste des patients après suppression
+        return $this->redirectToRoute('app_patient');
+    }
+
+    
+    /*Modifier un patient*/
+    #[Route('/modifierPatient/{id}', name: 'app_modifier_patient')]
+    public function modifierPatient(ManagerRegistry $doctrine, $id): Response
+    {
+        // Logique pour modifier un patient
+        return $this->render('administratif/modifier_patient.html.twig', [
+            'patientId' => $id,
+        ]);
+    }           
+
+
+
+    //Partie Séjour
     #[Route('/sejour', name: 'app_sejour')]
     public function sejours(): Response
     {
